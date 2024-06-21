8259fa9e7b4f834caa00e1354c89b099cfbf8318
Omar <omar.sayyid134@gmail.com>
2024-06-21 13:40:49 -0700

    betty documentaion

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,24 @@
 #include <stdio.h>
 #include <time.h>
 
-
-/* fucntion to determine if a random number is >, <, = zero */
+/**
+ * main - Determines if a random number is greater than, less than, or equal to zero
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		 printf("%d is zero\n", n);
-	else
-		if (n > 0)
-			printf("%d is positive\n", n);
-		
-		else
-			printf("%d is negative\n", n);
-	return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n == 0)
+        printf("%d is zero\n", n);
+    else if (n > 0)
+        printf("%d is positive\n", n);
+    else
+        printf("%d is negative\n", n);
+
+    return (0);
 }
+
