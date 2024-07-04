99694f20e6b87df82aa1203257447f58cd18b233
Omar <omar.sayyid134@gmail.com>
2024-07-04 09:07:29 -0700

    add numbers putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single numbers
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '1'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
