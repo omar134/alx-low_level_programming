e96acb42025a35fb2f765098023e533174424ac6
root <root@OmarThinkpad.localdomain>
2024-07-07 15:30:09 +0300

    betty stylized

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
 /**
  * main - prints a specific msg
- * 
+ *
  * Return: 1
  */
-int main(void) {
-    const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 59);
-    return 1;
+int main(void)
+{
+	const char *message = "and that piece of art is useful\"
+
+	- Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 59);
+	return (1);
 }
