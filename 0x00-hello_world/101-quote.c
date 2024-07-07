d38e44b123b472f27d3420a040b2c70fa2cc418d
root <root@OmarThinkpad.localdomain>
2024-07-07 15:35:20 +0300

    trial 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,8 @@
  * Return: 1
  */
 int main(void)
-{
-	const char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+{       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, message, 59);
-	return (1);
+        write(1, quo, 59);
+        return (1);
 }
