54be80a4c50674bc9b2de467d2be1ddad1476b19
root <root@OmarThinkpad.localdomain>
2024-07-07 15:33:48 +0300

    trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,8 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\"
+	const char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 
-	- Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, message, 59);
 	return (1);
 }
