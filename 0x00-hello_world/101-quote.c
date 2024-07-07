40377408fa49417cd2fbcdc3426e518f8a06e2aa
root <root@OmarThinkpad.localdomain>
2024-07-07 15:26:27 +0300

    fixed a comment

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void) {
     const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, message, 59); // 59 is the length of the message
+    write(STDERR_FILENO, message, 59);
     return 1;
 }
