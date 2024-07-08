2e1ea832156d04b0d89b8619d1fc177a702be107
Omar <omar.sayyid134@gmail.com>
2024-07-08 12:01:13 -0700

    trail

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -21,18 +21,19 @@ int main(void)
 {
 	unsigned int limit = 1024;
 	unsigned int sum = 0;
+	unsigned int i;
 
-	for (unsigned int i = 3; i < limit; i += 3)
+	for (i = 3; i < limit; i += 3)
 	{
 		sum += i;
 	}
 
-	for (unsigned int i = 5; i < limit; i += 5)
+	for (i = 5; i < limit; i += 5)
 	{
 		sum += i;
 	}
 
-	for (unsigned int i = 15; i < limit; i += 15)
+	for (i = 15; i < limit; i += 15)
 	{
 		sum -= i;
 	}
